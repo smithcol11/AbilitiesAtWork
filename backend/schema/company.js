3f4b3dedcbc435d3393ffae9044a0573cf64b04a
JunyiF <junyifeng98@gmail.com>
2022-11-21 16:49:54 -0800

    Update company.js

--- backend/schema/company.js
+++ backend/schema/company.js
@@ -6,8 +6,8 @@ const CompanySchema = new Schema({
     Company_ID: Schema.Types.ObjectId,
     Company_Name: String, 
     Address: String,
-    Contact_Name: String,
-    Contact_Info: [{
+    Contact_Info:[{
+        Contact_Name: String,
         Email: {
             type: String,
             trim: true,
@@ -16,19 +16,18 @@ const CompanySchema = new Schema({
             required: 'Email address is required',
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
         },
-        Bussiness_Phone: {
+        clearPhone: {
             type: String,
             required: 'Bussiness phone number is required',
             match: [/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/, 'Please fill a valid phone number'],
             /*this regex works for the formats (123) 456-7890 or 123-456-7890*/
         }, 
-        Other: String
+        Other: String,
     }],
-    Job_List: [{ type: Schema.Types.ObjectId, ref: 'Jobs' }]
-
+    Job_List: [{type: Schema.Types.ObjectId, ref: 'Jobs' }]
 });
 
 
 var Company  = mongoose.model('Company', CompanySchema);
-var Jobs  = mongoose.model('Jobs', JobSchema);
+/*var Jobs  = mongoose.model('Jobs', JobSchema);*/
 
