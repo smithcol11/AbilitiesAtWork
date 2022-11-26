8469d216ffec1cb4f56c7ee7e4fffbf6d7d68808
Colin Smith <smithcol@pdx.edu>
2022-11-26 10:54:31 -0800

    Modified the tailwind config to have variables for our theme colors. I tried following the docs to do this, but either I would have the default colors, or ONLY the theme colors. I found a solution to this by manually importing all the default colors as well as our theme.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,41 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+
+      light: "#f8fafc",
+      accentLight: "#a78bfa",
+      main: "#c026d3",
+      accentDark: "#5b21b6",
+      dark: "#0f172a",
+    },
     extend: {},
   },
   plugins: [],
