16e8c5fb936fbd777e69cb90e5d62b910c5a37b3
JunyiF <junyifeng98@gmail.com>
2022-11-26 14:29:45 -0800

    Update company.js

--- backend/schema/company.js
+++ backend/schema/company.js
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
-mongoose.connect("http://localhost:3000")
 const { Schema } = mongoose;
 
-const CompanySchema = new Schema({
-    Company_ID: Schema.Types.ObjectId,
-    Company_Name: String, 
+
+let company  = mongoose.model('company', companySchema);
+let job  = mongoose.model('job', jobSchema);
+
+const companySchema = new Schema({
+    companyID: Schema.Types.ObjectId,
+    companyName: String, 
     Address: String,
-    Contact_Info:[{
-        Contact_Name: String,
-        Email: {
+    contactInfo:[{
+        contactName: String,
+        email: {
             type: String,
             trim: true,
             lowercase: true,
@@ -16,18 +19,16 @@ const CompanySchema = new Schema({
             required: 'Email address is required',
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
         },
-        Phone: {
+        phone: {
             type: String,
             required: 'Bussiness phone number is required',
             match: [/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/, 'Please fill a valid phone number'],
             /*this regex works for the formats (123) 456-7890 or 123-456-7890*/
         }, 
-        Other: String,
+        other: String,
     }],
-    Job_List: [{type: Schema.Types.ObjectId, ref: 'Jobs' }]
+    jobList: [{type: Schema.Types.ObjectId, ref: 'job' }]
 });
 
 
-var Company  = mongoose.model('Company', CompanySchema);
-/*var Jobs  = mongoose.model('Jobs', JobSchema);*/
 
