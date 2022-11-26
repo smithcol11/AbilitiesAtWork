8123ab07fffdb5f3715f1da109af6b789e1a8b52
Colin Smith <smithcol@pdx.edu>
2022-11-26 11:52:17 -0800

    Made minor changed to the hover effect.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,26 +1,45 @@
 <script setup></script>
 <template>
-    <nav>
-        <div class="sm:w-32  sm:h-screen shadow-md text-center px-1 py-5">
-        <ul class="sm:pl-5 grid grid-cols-5 sm:grid-cols-1 text-xs text-gray-700 duration-300 hover:text-gray-900">
-            <li class="">
-                <RouterLink class="sm:flex items-center  py-4" to="/clientmenu">Client Menu</RouterLink>
-            </li>
-            <li class="">
-                <RouterLink class="sm:flex items-center  py-4" to="/jobmenu">Job Menu</RouterLink>
-            </li>
-            <li class="">
-                <RouterLink class="sm:flex items-center  py-4" to="/search">Search</RouterLink>
-            </li>
-            <li class="">
-                <RouterLink class="sm:flex items-center  py-4" to="/match">Match</RouterLink>
-            </li>
-            <li class="">
-                <RouterLink class="sm:flex items-center  py-4" to="/logout">Logout</RouterLink>
-            </li>
-        </ul>
+  <nav>
+    <div class="sm:w-32 sm:h-screen shadow-md text-center px-1 py-5">
+      <ul class="sm:pl-5 grid grid-cols-5 sm:grid-cols-1 text-xs text-black">
+        <li class="">
+          <RouterLink
+            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
+            to="/clientmenu"
+            >Client Menu</RouterLink
+          >
+        </li>
+        <li class="">
+          <RouterLink
+            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
+            to="/jobmenu"
+            >Job Menu</RouterLink
+          >
+        </li>
+        <li class="">
+          <RouterLink
+            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
+            to="/search"
+            >Search</RouterLink
+          >
+        </li>
+        <li class="">
+          <RouterLink
+            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
+            to="/match"
+            >Match</RouterLink
+          >
+        </li>
+        <li class="">
+          <RouterLink
+            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
+            to="/logout"
+            >Logout</RouterLink
+          >
+        </li>
+      </ul>
     </div>
-    </nav>
+  </nav>
 </template>
 <style></style>
-  
