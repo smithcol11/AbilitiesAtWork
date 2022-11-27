92339a5585b4d6588e2ec5552e37595268bdf5a6
Colin Smith <smithcol@pdx.edu>
2022-11-27 10:18:10 -0800

    Made minor changes to schema. Changed export name, removed 'job' in field names, edited shift based on Ian feedback, and added edited fields.

--- backend/schema/Job.js
+++ backend/schema/Job.js
@@ -1,25 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const jobSchema = new Schema({
-    job_id: { type: String, required: true },
-    posted_by: { type: String, required: true },
-    posted_on: { type: Date, required: true },
+const jobSchema = new Schema(
+  {
+    id: { type: String, required: true },
+    postedBy: { type: String, required: true },
+    postedOn: { type: Date, required: true },
+    editedBy: { type: String, required: true },
+    editedOn: { type: Date, required: true },
 
     detail: {
-        job_category:   { type: String, required: true },
-        job_type:       { type: ["Full-Time", "Part-Time"], required: true },  // this was originally worded "Hours per W" from the schema document
-        shift_schedule: { type: ["8-hour shift", "Day shift", "Morning shift", "Weekend availability", "Monday to Friday"], required: true },
-        pay:            { type: Number, required: true },
-        benefits:       { type: String } ,
-        notes:          { type: String }         //description
+      category: { type: String, required: true },
+      type: { type: ["Full-Time", "Part-Time"], required: true }, // this was originally worded "Hours per W" from the schema document
+      shift: {
+        // start times
+        type: [
+          "Early", // early, before 9
+          "Morning", // 9-12
+          "Afternoon", // 12-5
+          "Evening", // late, after 5
+        ],
+        required: true,
+      },
+      pay: { type: Number, required: true },
+      benefits: { type: String },
+      notes: { type: String }, //description
     },
-
+    address: { type: String, required: true },
+    city: { type: String, required: true },
     county: { type: String, required: true },
-    city: { type: String, required: true },     //added field 
-},
-    { timestamps: true }
-)
+  },
+  { timestamps: true }
+);
 
-const Message = mongoose.model("Message", messageSchema);
-module.exports = Message;
+const Job = mongoose.model("Job", jobSchema);
+module.exports = Job;
