ad55bb4ed377d4a8bb1f7184f016c401d3e73e30
Colin Smith <smithcol@pdx.edu>
2022-11-27 12:13:56 -0800

    Changed text size when screen gets bigger.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,7 +2,9 @@
 <template>
   <nav>
     <div class="sm:w-32 sm:h-screen shadow-md text-center px-1 py-5">
-      <ul class="sm:pl-5 grid grid-cols-5 sm:grid-cols-1 text-xs text-black">
+      <ul
+        class="sm:pl-5 grid grid-cols-5 sm:grid-cols-1 text-xs sm:text-sm text-black"
+      >
         <li class="">
           <RouterLink
             class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
