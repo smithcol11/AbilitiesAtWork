abe9a72e9adf72c3d102d82a5d64f0fe99be7607
JunyiF <junyifeng98@gmail.com>
2022-11-27 13:09:07 -0800

    Update company.js
    Update format, add export

--- backend/schema/company.js
+++ backend/schema/company.js
@@ -8,27 +8,22 @@ let job  = mongoose.model('job', jobSchema);
 const companySchema = new Schema({
     companyID: Schema.Types.ObjectId,
     companyName: String, 
-    Address: String,
-    contactInfo:[{
-        contactName: String,
-        email: {
-            type: String,
-            trim: true,
-            lowercase: true,
-            unique: true,
-            required: 'Email address is required',
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-        },
-        phone: {
-            type: String,
-            required: 'Bussiness phone number is required',
-            match: [/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/, 'Please fill a valid phone number'],
-            /*this regex works for the formats (123) 456-7890 or 123-456-7890*/
-        }, 
-        other: String,
-    }],
-    jobList: [{type: Schema.Types.ObjectId, ref: 'job' }]
+    address: String,
+    contactName: String,
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: true,
+    },
+    phone: {
+        type: Number,
+        trim: true,
+        required: true,
+    }, 
+    other: String,
+    jobs: [{type: Schema.Types.ObjectId, ref: 'job' }]
 });
 
-
-
+module.exports = company;
