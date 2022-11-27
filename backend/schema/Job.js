0c27edb5fa68068650a55b833a53dafcc69f5be4
benttruong <108073148+benttruong@users.noreply.github.com>
2022-11-27 09:43:38 -0800

    updating format and adding export

--- backend/schema/Job.js
+++ backend/schema/Job.js
@@ -2,23 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const jobSchema = new Schema({
-    job_id: String,
-    required: true,
+    job_id: { type: String, required: true },
+    posted_by: { type: String, required: true },
+    posted_on: { type: Date, required: true },
 
-    posted_by: String,
-    
-    posted_on: Date,
-    required: true,
-    
     detail: {
-        job_category: String,
-        job_type: ["Full-Time", "Part-Time"],   // this was originally worded "Hours per W" from the schema document
-        shift_schedule: ["8-hour shift", "Day shift", "Morning shift", "Weekend availability", "Monday to Friday"],
-        pay: Number,
-        benefits: String,
-        notes: String                           //description
+        job_category:   { type: String, required: true },
+        job_type:       { type: ["Full-Time", "Part-Time"], required: true },  // this was originally worded "Hours per W" from the schema document
+        shift_schedule: { type: ["8-hour shift", "Day shift", "Morning shift", "Weekend availability", "Monday to Friday"], required: true },
+        pay:            { type: Number, required: true },
+        benefits:       { type: String } ,
+        notes:          { type: String }         //description
     },
 
-    county: String,
-    city: String                                //added field 
-})
+    county: { type: String, required: true },
+    city: { type: String, required: true },     //added field 
+},
+    { timestamps: true }
+)
+
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
