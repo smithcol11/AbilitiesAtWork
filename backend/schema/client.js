e7c9e452618a6ce0f83ce67e2aba43fa9fdc9a9c
JunyiF <junyifeng98@gmail.com>
2022-12-10 17:45:47 -0800

    Update client.js

--- backend/schema/client.js
+++ backend/schema/client.js
@@ -1,30 +1,3 @@
-/*
-//Client schema file.
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const clientSchema = new Schema({
-  userID: {type: Number, required: true},
-  initials: {type: String, required: true, minlength: 2},
-  preference: {
-    industry: [{type: String, required: true}],
-    hours: {type: String, enum: ['Any', 'Part-Time', 'Full-Time'], default: 'Any', required: true}
-  }, 
-  savedJobs: String,
-  appliedJobs: String, //How are saved jobs handled?
-  postedBy: { type: String, required: true },
-  postedOn: {type: Date, default: Date.now, required: true},
-  editedBy: { type: String, required: true },
-  editedOn: {type: Date, default: Date.now},
-
-});
-const Client = mongoose.model(
-  "Client",
-  clientSchema
-  );
-module.exports = Client;
-*/
-
 //Client schema file.
 
 import mongoose from 'mongoose';
