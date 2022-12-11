5630f8d28b362acb754313a105eb03144d48876b
JunyiF <junyifeng98@gmail.com>
2022-12-10 17:42:38 -0800

    Update client.js

--- backend/schema/client.js
+++ backend/schema/client.js
@@ -1,13 +1,41 @@
+/*
+//Client schema file.
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const clientSchema = new Schema({
+  userID: {type: Number, required: true},
+  initials: {type: String, required: true, minlength: 2},
+  preference: {
+    industry: [{type: String, required: true}],
+    hours: {type: String, enum: ['Any', 'Part-Time', 'Full-Time'], default: 'Any', required: true}
+  }, 
+  savedJobs: String,
+  appliedJobs: String, //How are saved jobs handled?
+  postedBy: { type: String, required: true },
+  postedOn: {type: Date, default: Date.now, required: true},
+  editedBy: { type: String, required: true },
+  editedOn: {type: Date, default: Date.now},
+
+});
+const Client = mongoose.model(
+  "Client",
+  clientSchema
+  );
+module.exports = Client;
+*/
+
 //Client schema file.
 
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const clientSchema = new Schema({
-  userID: {type: Integer, required: true},
+  userID: {type: Number, required: true},
   initials: {type: String, required: true, minlength: 2},
   preference: {
-    industry: {type: [String], required: true},
+    industry: [{type: String, required: true}],
+
     hours: {type: String, enum: ['Any', 'Part-Time', 'Full-Time'], default: 'Any', required: true}
   }, 
   savedJobs: String,
@@ -23,4 +51,4 @@ const Client = mongoose.model(
   "Client",
   clientSchema
   );
-modeule.exports = Client;
+module.exports = Client;
