e3f2f11a0016f11ac6d923dc77871d152dc3b9d1
benttruong <108073148+benttruong@users.noreply.github.com>
2022-12-12 09:58:13 -0800

    changing syntax of document

--- backend/__test__/job.spec.js
+++ backend/__test__/job.spec.js
@@ -5,28 +5,28 @@ import { connectDatabase, finalizeTest, setupTest } from "./db-test";
 import { Error } from "mongoose";
 import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 
-const exampleJob = {
-    id: "12345",
-    postedBy: "John Doe",
-    postedOn: 03/20/2022, 
-    editedBy: "Jane Doe",
-    editedOn: 03/31/2022,
+// const exampleJob = {
+//     id: "12345",
+//     postedBy: "John Doe",
+//     postedOn: 03/20/2022, 
+//     editedBy: "Jane Doe",
+//     editedOn: 03/31/2022,
   
-    detail: {
-        category: "Delivery", // { type: String, required: true },
-        type: "Full-Time", // { type: String, enum: ["Full-Time", "Part-Time"], required: true }, // this was originally worded "Hours per W" from the schema document
-        shift: "Morning",
-        pay: 20.00, 
-        benefits: "Paid time off",
-        notes: "n/a", //description
-      },
-    address: "123 Main st", 
-    city: Portland,
-    county: "Multnomah", 
-    // { timestamps: true }    
-};
+//     detail: {
+//         category: "Delivery", // { type: String, required: true },
+//         type: "Full-Time", // { type: String, enum: ["Full-Time", "Part-Time"], required: true }, // this was originally worded "Hours per W" from the schema document
+//         shift: "Morning",
+//         pay: 20.00, 
+//         benefits: "Paid time off",
+//         notes: "n/a", //description
+//       },
+//     address: "123 Main st", 
+//     city: Portland,
+//     county: "Multnomah", 
+//     // { timestamps: true }    
+// };
 
-/*
+const exampleJob =
 {
   "_id": {
     "$oid": "639766bb2f6540b8d0711ec2"
@@ -47,7 +47,7 @@ const exampleJob = {
     }
   }
 }
-*/
+
 
 let session = null;
 
