9eae8e6742560dca9e37ecdbd26d0eaaf117a683
jdustin2 <jdustin@pdx.edu>
2022-12-14 13:28:22 -0800

    Style navbar for mobile and desktop
    Add hover and active effects to mobile and desktop view, allow opening
    and closing of mobile navbar, add AAW image to desktop view.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,47 +1,32 @@
-<script setup></script>
+<script setup>
+function toggleNav() {
+  let nav = document.getElementById("nav-bar");
+  nav.classList.toggle("hidden");
+}
+</script>
 <template>
-  <nav>
-    <div class="sm:w-32 sm:h-screen shadow-md text-center px-1 py-5">
-      <ul
-        class="sm:pl-5 grid grid-cols-5 sm:grid-cols-1 text-xs sm:text-sm text-black"
-      >
-        <li class="">
-          <RouterLink
-            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
-            to="/clientmenu"
-            >Client Menu</RouterLink
-          >
-        </li>
-        <li class="">
-          <RouterLink
-            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
-            to="/jobmenu"
-            >Job Menu</RouterLink
-          >
-        </li>
-        <li class="">
-          <RouterLink
-            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
-            to="/search"
-            >Search</RouterLink
-          >
-        </li>
-        <li class="">
-          <RouterLink
-            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
-            to="/match"
-            >Match</RouterLink
-          >
-        </li>
-        <li class="">
-          <RouterLink
-            class="sm:flex items-center py-4 hover:text-gray-500 duration-300"
-            to="/logout"
-            >Logout</RouterLink
-          >
-        </li>
-      </ul>
+  <nav id="nav-bar" class="hidden sm:block duration-300">
+    <div class="sm:w-32 sm:h-screen shadow-sm text-center bg-light ">
+      <img class="hidden sm:block" src="../assets/images/logo.png">
+      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light"
+        to="/clientmenu">Client Menu
+      </RouterLink>
+      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/jobmenu">
+        Job Menu</RouterLink>
+      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/search">
+        Search</RouterLink>
+      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/match">
+        Match</RouterLink>
+      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/logout">
+        Logout</RouterLink>
     </div>
   </nav>
+  <button class="sm:hidden flex mx-auto border-b border-x shadow-l bg-light" @click="toggleNav()">
+    <svg class="h-6 w-24 text-accentDark" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
+    </svg>
+  </button>
 </template>
-<style></style>
+<style>
+
+</style>
