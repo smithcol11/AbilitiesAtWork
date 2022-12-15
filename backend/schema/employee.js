d0687cce7eb13d41307dc7a86e77dabda151b58b
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2022-12-14 17:15:45 -0800

    separate the employee and admin to two schemas

---
+++ backend/schema/employee.js
@@ -0,0 +1,16 @@
+
+import mongoose from 'mongoose';
+const passportLocalMongoose = require('passport-local-mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        first: { type: String, required: true, minlength: 1 },
+        last: { type: String, required: true, minlength: 1 },
+    }
+);
+
+userSchema.plugin(passportLocalMongoose)
+const Employee = mongoose.model('Employee', userSchema);
+
+module.exports = Employee;
