47b6b844d8c5f2ebf38169e45ccf2d2f426f43a0
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2022-12-15 03:20:28 -0800

    replace import mongoose to require in employee.js

--- backend/schema/admin.js
+++ backend/schema/admin.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const passportLocalMongoose = require('passport-local-mongoose');
 const { Schema } = mongoose;
 
--- backend/schema/employee.js
+++ backend/schema/employee.js
@@ -1,5 +1,5 @@
+const mongoose = require("mongoose");
 
-import mongoose from 'mongoose';
 const passportLocalMongoose = require('passport-local-mongoose');
 const { Schema } = mongoose;
 
