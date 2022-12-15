92ed30782102c99bd0efc2191b09fe805916c1a3
jdustin2 <jdustin@pdx.edu>
2022-12-15 13:54:59 -0800

    Add padding and border styles, reduce image size

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -7,23 +7,26 @@ function toggleNav() {
 <template>
   <nav id="nav-bar" class="hidden sm:block duration-300">
     <div class="sm:w-32 sm:h-screen shadow-sm text-center bg-light ">
-      <img class="hidden sm:block" src="../assets/images/logo.png">
-      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light"
+      <img class="p-3 border-b-2 hidden sm:block" src="../assets/images/logo.png">
+      <RouterLink class="py-2 border-b-2 sm:py-3 text-center flex justify-center hover:bg-accentDark hover:text-light"
         to="/clientmenu">Client Menu
       </RouterLink>
-      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/jobmenu">
+      <RouterLink class="py-2 border-b-2 sm:py-3 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/jobmenu">
         Job Menu</RouterLink>
-      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/search">
+      <RouterLink class="py-2 border-b-2 sm:py-3 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/search">
         Search</RouterLink>
-      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/match">
+      <RouterLink class="py-2 border-b-2 sm:py-3 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/match">
         Match</RouterLink>
-      <RouterLink class=" sm:py-6 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/logout">
+      <RouterLink class="py-2 border-b-2 sm:py-3 text-center flex justify-center hover:bg-accentDark hover:text-light" to="/logout">
         Logout</RouterLink>
     </div>
   </nav>
   <button class="sm:hidden flex mx-auto border-b border-x shadow-l bg-light" @click="toggleNav()">
-    <svg class="h-6 w-24 text-accentDark" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
+    <svg class="h-7 w-32 text-dark" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+      stroke-linecap="round" stroke-linejoin="round">
+      <line x1="3" y1="12" x2="21" y2="12" />
+      <line x1="3" y1="6" x2="21" y2="6" />
+      <line x1="3" y1="18" x2="21" y2="18" />
     </svg>
   </button>
 </template>
