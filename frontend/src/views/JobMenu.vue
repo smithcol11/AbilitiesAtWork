6c051707584901d4f8cb46176aba0fbd7b87e043
Richard Duffy <rduffy@pdx.edu>
2022-12-16 11:57:48 -0500

    changed buttons and fixed height

--- frontend/src/views/JobMenu.vue
+++ frontend/src/views/JobMenu.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-  <div class="Job Menu text-2xl rounded w-1/3 h-2/3 bg-light p-5 shadow-lg border">
+  <div class="Job Menu text-2xl rounded w-1/3 h-100 bg-light p-5 shadow-lg border">
     <h1>Job Menu</h1>
     <div class = "pt-6 text-lg">
     <p>Please select from the following options:</p>
@@ -8,7 +8,7 @@
     <div class = "pt-5">
       <button
         type="add"
-        class="duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
         @click=""
       >
         Add Job
@@ -17,7 +17,7 @@
       <div class = "pt-5">
       <button
         type="remove"
-        class="duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
         @click=""
       >
         Remove Job
@@ -26,7 +26,7 @@
       <div class = "pt-5">
       <button
         type="edit"
-        class="duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
         @click=""
       >
         Edit Job
@@ -35,7 +35,7 @@
       <div class = "pt-5">
       <button
         type="all"
-        class="duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 duration-300 bg-accentDark hover:bg-accentLight px-4 py-1 my-1 font-bold text-base text-light hover:text-dark rounded"
         @click=""
       >
         View All Jobs
