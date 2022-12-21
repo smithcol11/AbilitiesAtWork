8253100f09f5df2ded58b46dcc44b8e90efe3ef6
Richard Duffy <rduffy@pdx.edu>
2022-12-21 15:11:22 -0500

    improved some size styling

--- frontend/src/views/JobMenu.vue
+++ frontend/src/views/JobMenu.vue
@@ -10,12 +10,12 @@ const JobOption = ref(0);
 
 </script>
 <template>
-  <div class="Job Menu mx-auto text-2xl rounded w-1/3 h-100 bg-light p-5 shadow-lg text-center border">
+  <div class="Job Menu mx-auto text-2xl rounded w-2/3 h-100 bg-light p-5 shadow-lg text-center border">
     <h1>Job Menu</h1>
     <div class = "pt-6 text-lg">
     <p>Please select from the following options:</p>
     </div>
-    <div class = "pt-3 py-3 md:pt-5 md:py-5 lg:pt-0 lg:py-0 mx-auto">
+    <div class = "pt-3 py-3 md:pt-5 md:py-5 lg:pt-0 lg:py-0 mx-auto ">
       <router-link
       to="/AddJob"
       custom
@@ -23,7 +23,7 @@ const JobOption = ref(0);
     >
       <button
         type="add"
-        class=" w-20 bg-accentDark pt-1 py-1 md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-30 sm:bg-accentDark duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 bg-accentDark pt-1 py-1 md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-30 sm:bg-accentDark duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
         @click="navigate"
       >
         Add Job
@@ -38,7 +38,7 @@ const JobOption = ref(0);
     >
       <button
         type="remove"
-        class=" w-20 bg-accentDark pt-1 py-1 md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-20 sm:bg-accentDark  duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 bg-accentDark pt-1 py-1  md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-40 sm:bg-accentDark  duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
         @click="navigate"
       >
         Remove Job
@@ -53,7 +53,7 @@ const JobOption = ref(0);
     >
       <button
         type="add"
-        class=" w-20 bg-accentDark pt-1 py-1 md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-30 sm:bg-accentDark duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 bg-accentDark pt-1 py-1 md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-30 sm:bg-accentDark duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
         @click="navigate"
       >
         Edit Job
@@ -69,7 +69,7 @@ const JobOption = ref(0);
     >
       <button
         type="add"
-        class=" w-20 bg-accentDark pt-1 py-1 md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-30 sm:bg-accentDark duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
+        class=" w-40 bg-accentDark pt-1 py-1 md:pt-2 md:py-2 lg:pt-2 lg:py-2 lg:px-4 lg:py-1 lg:my-5 lg:w-40 md:w-40 sm:w-30 sm:bg-accentDark duration-300 lg:bg-accentDark hover:bg-accentLight font-bold text-base text-light hover:text-dark rounded"
         @click="navigate"
       >
         View All Jobs
