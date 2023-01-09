494cdc92c6a7bc4769c456362137660fca6933ab
JunyiF <junyifeng98@gmail.com>
2023-01-09 15:53:14 -0800

    add simple rows for testing

---
+++ frontend/src/components/Row.vue
@@ -0,0 +1,111 @@
+ 
+ 
+<template>
+    
+    <!--
+    <div v-for="job_alias in Jobs" 
+    class="text-red-500 flex flex-row">
+        <div class="grow">{{job_alias.organization}}</div>
+        <div class="grow">{{job_alias.detail.category}}</div>
+        <div class="grow">{{job_alias.detail.type}}</div>
+        <div>{{job_alias.detail.shift}}</div>
+        <div>{{job_alias.county}}</div>
+    </div>
+    
+
+    <tbody class="table-auto">
+        <tr>
+            <td>{{job_alias.organization}}</td>
+            <td>{{job_alias.detail.category}}</td>
+            <td>{{job_alias.detail.type}}</td>
+            <td>{{job_alias.detail.shift}}</td>
+            <td>{{job_alias.county}}</td>
+        </tr>
+    </tbody>
+    -->
+    <table class="w-full border-collapse border p-2 text-xl">
+
+        <tbody>
+            <tr v-for="job_alias in Jobs"
+            class="bg-white even:bg-gray-300">
+                <td >{{job_alias.organization}}</td>
+                <td >{{job_alias.detail.category}}</td>
+                <td >{{job_alias.detail.type}}</td>
+                <td >{{job_alias.detail.shift}}</td>
+                <td >{{job_alias.county}}</td>
+            </tr>
+    </tbody>
+</table>    
+
+</template>
+ 
+<script>
+export default{
+    name: 'job',
+    data() {
+        return{
+            Jobs:[],
+        }
+    },
+    mounted(){
+        //Example jobs for testing
+        this.Jobs = {
+            "exampleJob1": {
+                "id": 12345,
+                "organization": "Ross",
+                "detail": {
+                    "category": "Delivery",
+                    "type": "Full-Time",
+                    "shift": "Morning",
+                    "pay": 20.0,
+                    "benefits": "Paid time off",
+                    "notes": "n/a",
+                },
+                "address": "123 Main st",
+                "city": "Portland",
+                "county": "Multnomah",
+            },
+            "exampleJob2": {
+                "id": 54321,
+                "organization": "Vet Test",
+                "detail": {
+                    "category": "Clerk",
+                    "type": "Part-Time",
+                    "shift": "Afternoon",
+                    "pay": 18.0,
+                    "benefits": "Health insurance",
+                    "notes": "n/a",
+                },
+                "address": "1 First st",
+                "city": "Hillsboro",
+                "county": "Washington",
+            },
+            "exampleJob3": {
+                "id": 1,
+                "organization": "Store Test",
+                "detail": {
+                    "category": "Sales",
+                    "type": "Part-Time",
+                    "shift": "Evening",
+                    "pay": 15.75,
+                    "benefits": "",
+                    "notes": "n/a",
+                },
+                "address": "1 First st",
+                "city": "Portland",
+                "county": "Multnomah",
+            },
+            
+        }
+}
+       
+}
+ 
+ 
+ 
+</script>
+ 
+<style>
+ 
+</style>
+
--- frontend/src/views/Match.vue
+++ frontend/src/views/Match.vue
@@ -1,8 +1,13 @@
+<script setup>
+import Row from "../components/Row.vue";
+</script>
+
 
 <template>
   <div class="Match">
     <h1>This is the Match page</h1>
   </div>
+  <Row></Row>
 </template>
 
 <style></style>
