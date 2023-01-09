8b86223b2c8482e08e435ec61792108fb0e71c29
Jacob Padgett <jacobapadgett@gmail.com>
2023-01-08 22:13:36 -0800

    Testing out vueform-multiselect

---
+++ frontend/src/components/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Multiselect
+      class="vueform-multiselect"
+      v-model="value"
+      mode="tags"
+      :close-on-select="false"
+      :groups="true"
+      :options="options"
+    />
+  </div>
+</template>
+
+<script>
+  import Multiselect from '@vueform/multiselect'
+
+  export default {
+    components: {
+      Multiselect,
+    },
+    data() {
+      return {
+        value: null,
+        options: [
+          {
+            label: 'SELECT ALL',
+            options: ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6'],
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
