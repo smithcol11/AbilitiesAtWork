11793298921a4c8a2f7278b2fb333b067ca7e153
Cesium1289 <Carson.hansen@gmail.com>
2023-01-08 16:47:10 -0800

    Created CreateJob view
    Added the CreateJob view page to start the process of adding the needed fields

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -32,6 +32,11 @@ const router = createRouter({
       name: "login",
       component: () => import("../views/Login.vue"),
     },
+    {
+      path: "/jobmenu/createjob",
+      name: "createjob",
+      component: () => import("../views/CreateJob.vue"),
+    },
   ],
 });
 export default router;
---
+++ frontend/src/views/CreateJob.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="CreateJob">
+    <h1>This is the Create Job page</h1>
+  </div>
+</template>
+
+<style></style>
