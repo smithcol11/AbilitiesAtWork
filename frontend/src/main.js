b48917ea900ea9b1eaa87c9cd1dce986cf35520c
benttruong <108073148+benttruong@users.noreply.github.com>
2023-01-12 05:15:45 -0800

    stable run after datepicker installation before implementation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,14 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component('Datepicker', Datepicker);
 
 app.mount("#app");
--- frontend/src/views/CreateJob.vue
+++ frontend/src/views/CreateJob.vue
@@ -1,37 +1,42 @@
 <script setup>
-import { ref } from "vue";
-const businessName = ref("");
-const contactName = ref("");
-const position = ref("");
-const contactInfo = ref("");
-let industries = [];
-const selectedIndustries = ref([]);
-const hours = ref(["Full-Time", "Part-Time", "Both"]);
-const selectedHours = ref("");
-const shifts = ref(["Morning", "Afternoon", "Night"]);
-const selectedShifts = ref([]);
-const selectedCounty = ref("");
-const address = ref("");
-const notes = ref("");
+  import { ref } from "vue";
+  import Datepicker from '@vuepic/vue-datepicker';
+  import '@vuepic/vue-datepicker/src/VueDatePicker/style/main.scss';
 
-//variables used in the first draft. I did not remove these
-//yet since the console will display warnings if they are not defined
-const industry = ref("");
-const message = ref("");
-const vald = ref(false);
-const datePosted = ref(null);
-const name = ref("");
-const shift = ref("");
+  const businessName = ref("");
+  const contactName = ref("");
+  const position = ref("");
+  const contactInfo = ref("");
+  let industries = [];
+  const selectedIndustries = ref([]);
+  const hours = ref(["Full-Time", "Part-Time", "Both"]);
+  const selectedHours = ref("");
+  const shifts = ref(["Morning", "Afternoon", "Night"]);
+  const selectedShifts = ref([]);
+  const selectedCounty = ref("");
+  const address = ref("");
+  const notes = ref("");
+  const datePosted = ref();
 
-//Will be used to fetch industries when industries are added to the database.
-//Used dummy data in the meantime
-function FetchIndustries() {
-  console.log("Called 'FetchIndustries' function");
-  industries = ["Cafe", "Retail", "Food", "Janitorial"];
-}
 
-//called functions when window is loaded
-window.onload = FetchIndustries();
+  //variables used in the first draft. I did not remove these
+  //yet since the console will display warnings if they are not defined
+  const industry = ref("");
+  const message = ref("");
+  const vald = ref(false);
+  const name = ref("");
+  const shift = ref("");
+
+  //Will be used to fetch industries when industries are added to the database.
+  //Used dummy data in the meantime
+  function FetchIndustries() {
+    console.log("Called 'FetchIndustries' function");
+    industries = ["Cafe", "Retail", "Food", "Janitorial"];
+  }
+
+  //called functions when window is loaded
+  window.onload = FetchIndustries();
+
 </script>
 
 <template>
@@ -118,8 +123,14 @@ window.onload = FetchIndustries();
             type="text"
             v-model="address"
             placeholder="Enter here"
-        />
+          />
         </div>
+
+        <div>
+          <span>Date Posted:</span>
+          <!-- <Datepicker v-model="datePosted"></Datepicker> -->
+        </div>
+
         <div>
           <span>Notes: </span>
           <input
@@ -130,63 +141,6 @@ window.onload = FetchIndustries();
           />
         </div>
       </div>
-      
-
-
-
-      <!--This is here to seperate what the first draft was from what is currently being created-->
-      <p>|</p>
-      <p>|</p>
-      <p>|</p>
-      <p>|</p>
-      <p>|</p>
-      <!-- <h1>This is the Create Job page </h1> -->
-      <span>Business Name: </span>
-      <input v-model="name" type="text" placeholder="" />
-      <span>Contact Name:</span>
-      <input v-model="name" type="text" placeholder="" /><br />
-      <span>Position:</span>
-      <input v-model="name" type="text" placeholder="" />
-      <span>Contact info:</span>
-      <input v-model="name" type="text" placeholder="" /><br />
-
-      <span>Industry:</span>
-      <input type="radio" v-model="industry" value="Cafe" />
-      <label>Cafe</label>
-      <input type="radio" v-model="industry" value="Retail" />
-      <label>Retail</label>
-      <input type="radio" v-model="industry" value="Food" />
-      <label>Food</label>
-      <input type="radio" v-model="industry" value="Janitorial" />
-      <label>Janitorial</label>
-      <button class="btn">Add Industry</button><br />
-
-      <span>Hours:</span>
-      <input type="radio" v-model="hours" value="Full-Time" />
-      <label>Full-Time</label>
-      <input type="radio" v-model="hours" value="Part-Time" />
-      <label>Part-Time</label>
-      <input type="radio" v-model="hours" value="Both" />
-      <label>Both</label><br />
-
-      <span>Shift(s):</span>
-      <input type="radio" v-model="shift" value="Morning" />
-      <label>Morning</label>
-      <input type="radio" v-model="shift" value="Afternoon" />
-      <label>Afternoon</label>
-      <input type="radio" v-model="shift" value="Night" />
-      <label>Night</label><br />
-
-      <span>Country:</span>
-      <input v-model="name" type="text" placeholder="" />
-      <span>Address</span>
-      <input v-model="name" type="text" placeholder="" /><br />
-
-      <span>Date Posted</span>
-      <input v-model="name" type="text" placeholder="" /><br />
-
-      <span>Notes:</span><br />
-      <textarea> </textarea><br />
 
       <br />
       <button class="btn">SUBMIT</button>
@@ -201,31 +155,3 @@ window.onload = FetchIndustries();
     <p>Selected county: {{ selectedCounty }}</p>
   </form>
 </template>
-<!--
-
-  <style>
-  input {
-    padding: auto;
-    margin: 4px;
-    border-bottom: 0em;
-  }
-  .btn {
-    background: rgb(206, 87, 198);
-    color: #fff;
-    padding: 10px 15px;
-    margin: 5px;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 12px;
-  }
-  
-  input {
-    border: 1px solid rgb(35, 173, 17);
-  }
-  
-  textarea {
-    border: 1px solid purple;
-  }
-</style>
-
--->
