8c75d63ab94cb1ae1d96293657a29b20c1ced4bf
Jonathan-PSU <jonatr35@pdx.edu>
2023-01-11 16:14:36 -0800

    Added PrimeVue table plugin, not currently working

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from 'primevue/config';
+import DataTable from 'primevue/datatable';
+
+
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +12,9 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue);
+app.component('DataTable', DataTable);
+
+
 
 app.mount("#app");
