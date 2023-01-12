c3a29a4f3b47770e941f80ab2a0d3c2e899261ab
Cesium1289 <Carson.hansen@gmail.com>
2023-01-12 14:05:39 -0800

    Added form validation
    Fixed validation for form components. Need to find a way to make sure one radio button or checkbox is selected. Also need to find a calendar library

--- frontend/src/views/CreateJob.vue
+++ frontend/src/views/CreateJob.vue
@@ -1,84 +1,56 @@
 <script setup>
-  import { ref } from "vue";
-  import datePick from '../components/DatePick.vue';
+import { ref } from "vue";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+const businessName = ref("");
+const contactName = ref("");
+const position = ref("");
+const contactInfo = ref("");
+let industries = [];
+const selectedIndustries = ref([]);
+const hours = ref(["Full-Time", "Part-Time", "Both"]);
+const selectedHours = ref("");
+const shifts = ref(["Morning", "Afternoon", "Night"]);
+const selectedShifts = ref([]);
+const selectedCounty = ref("");
+const address = ref("");
+const notes = ref("");
+const date = ref();
 
-  const businessName = ref("");
-  const contactName = ref("");
-  const position = ref("");
-  const contactInfo = ref("");
-  let industries = [];
-  const selectedIndustries = ref([]);
-  const hours = ref(["Full-Time", "Part-Time", "Both"]);
-  const selectedHours = ref("");
-  const shifts = ref(["Morning", "Afternoon", "Night"]);
-  const selectedShifts = ref([]);
-  const selectedCounty = ref("");
-  const address = ref("");
-  const notes = ref("");
-  const datePosted = ref();
+//Will be used to fetch industries when industries are added to the database.
+//Used dummy data in the meantime
+function FetchIndustries() {
+  console.log("Called 'FetchIndustries' function");
+  industries = ["Cafe", "Retail", "Food", "Janitorial"];
+}
 
-
-  //variables used in the first draft. I did not remove these
-  //yet since the console will display warnings if they are not defined
-  const industry = ref("");
-  const message = ref("");
-  const vald = ref(false);
-  const name = ref("");
-  const shift = ref("");
-
-  //Will be used to fetch industries when industries are added to the database.
-  //Used dummy data in the meantime
-  function FetchIndustries() {
-    console.log("Called 'FetchIndustries' function");
-    industries = ["Cafe", "Retail", "Food", "Janitorial"];
-  }
-
-
-    function submit(){
-      if (
-        businessName.value &&
-        contactName.value &&
-        position.value &&
-        contactInfo.value &&
-        selectedIndustries.value &&
-        selectedHours.value &&
-        selectedShifts.value &&
-        selectedCounty.value &&
-        address.value) {
-        alert("New Job Submited");
-      } else {
-        alert("Required field missing");
-      }
-     }
-
-  //called functions when window is loaded
-  window.onload = FetchIndustries();
-
+//called functions when window is loaded
+window.onload = FetchIndustries();
 </script>
 
 <template>
-  <form @submit.prevent="handleSubmit">
-    <div class="CreateJob">
+  <form @submit.prevent>
+    <div class="JobCreation">
       <div class="container ms space-y-2">
         <div class="flex flex-row gap-x-4">
           <div>
             <span>Business Name: </span>
             <input
-              required
               class="border-2"
               type="text"
               v-model="businessName"
               placeholder=""
+              required
             />
           </div>
           <div>
             <span>Contact Name: </span>
             <input
-              required
               class="border-2"
               type="text"
               v-model="contactName"
               placeholder=""
+              required
             />
           </div>
         </div>
@@ -86,27 +58,27 @@
           <div>
             <span>Position: </span>
             <input
-              required
               class="border-2"
               type="text"
               v-model="position"
               placeholder=""
+              required
             />
           </div>
           <div>
             <span>Contact Info: </span>
             <input
-              required
               class="border-2"
               type="text"
               v-model="contactInfo"
               placeholder=""
+              required
             />
           </div>
         </div>
         <div class="flex flex-wrap gap-x-4">
           <span>Industry:</span>
-          <div v-for="industry in industries">
+          <div v-for="industry in industries" :key="industry">
             <input
               type="checkbox"
               :value="industry"
@@ -115,36 +87,30 @@
             <span class="checkbox-label">{{ industry }}</span>
           </div>
         </div>
-
         <div class="flex flex-wrap gap-x-4">
           <span>Hours:</span>
-          <div v-for="hour in hours">
-            <input
-              required              
-              type="radio" 
-              :value="hour" 
-              v-model="selectedHours" 
-            />
+          <div v-for="hour in hours" :key="hour" required>
+            <input type="radio" :value="hour" v-model="selectedHours" />
             <span class="checkbox-label">{{ hour }}</span>
           </div>
         </div>
         <div class="flex flex-wrap gap-x-4">
           <span>Shift(s):</span>
-          <div v-for="shift in shifts">
+          <div v-for="shift in shifts" :key="shift">
             <input type="checkbox" :value="shift" v-model="selectedShifts" />
             <span class="checkbox-label">{{ shift }}</span>
           </div>
         </div>
         <span> County: </span>
-        <select required v-model="selectedCounty">
+        <select v-model="selectedCounty" required>
           <option disabled value="">Please select one</option>
           <option>Clackamas</option>
           <option>Multnomah</option>
           <option>Washington</option>
         </select>
         <div>
-        <span>Address: </span>
-         <input
+          <span>Address: </span>
+          <input
             required
             class="border-2"
             type="text"
@@ -155,28 +121,36 @@
 
         <div>
           <label>Date Posted: </label>
-          <!-- <datePick /> -->
-        </div>
-
-        <span>Notes (optional): </span>
-        <div>
-          <textarea
+          <input
+            required
             class="border-2"
             type="text"
+            v-model="date"
+            placeholder="(mm/dd/yyyy)"
+          />
+          <!--<Datepicker v-model="date" :enable-time-picker="false"></Datepicker>-->
+        </div>
+
+        <div>
+          <span>Notes: </span>
+          <p style="white-space: pre-line"></p>
+          <textarea
             v-model="notes"
-            placeholder=""
+            placeholder="add notes (optional)"
           ></textarea>
         </div>
       </div>
 
-      <br/>
+      <br />
       <div>
-        <button @click="submit"
-          class="bg-accentLight hover:bg-accentDark text-white font-bold py-2 px-4 rounded">
+        <button
+          @click="submit"
+          class="bg-accentLight hover:bg-accentDark text-white font-bold py-2 px-4 rounded"
+        >
           SUBMIT
         </button>
       </div>
-      <br/>
+      <br />
     </div>
     <span>---------------------------------------------</span>
     <p>businessName: {{ businessName }}</p>
