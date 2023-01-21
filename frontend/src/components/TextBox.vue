a237d2a667cc1de50640aee50b02960591d075ce
Cesium1289 <Carson.hansen@gmail.com>
2023-01-21 10:18:32 -0800

    Fixed coloring of textbox

--- frontend/src/components/TextBox.vue
+++ frontend/src/components/TextBox.vue
@@ -7,7 +7,7 @@ const data = ref("");
 <template>
   <input
     type="type"
-    class="form-control block w-full px-3 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-accentLight rounded"
+    class="form-control block w-full px-3 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-none rounded"
     placeholder="placeholder"
     v-model="data"
   />
