727c4d7d97239e02dde9070507303bba9f3aa922
Cesium1289 <Carson.hansen@gmail.com>
2023-01-26 13:56:00 -0800

    Added form banenrs
    -Created failed/ success banners based on user input
    -Reset form values when user enters all successful data
    -Remade dropdown/ textbox components to follow vue 3.0 model

--- frontend/src/components/DropDown.vue
+++ frontend/src/components/DropDown.vue
@@ -1,15 +1,23 @@
 <script setup>
 import Multiselect from "@vueform/multiselect";
-import { ref } from "vue";
-const options = ref(["OPTION-1", "OPTION-2"]);
-const choice = ref("Choice");
-const placeholder = ref("");
+const props = defineProps({
+  options: {
+    default: ["OPTION-1", "OPTION-2"],
+  },
+  placeholder: {
+    type: String,
+    default: "Place holder text",
+  },
+  label: {
+    type: String,
+    default: "",
+  },
+});
 </script>
-<style src="@vueform/multiselect/themes/default.css"></style>
+
 <template>
   <multiselect
     class="vueform-multiselect"
-    v-model="choice"
     :options="options"
     :clear-on-select="true"
     :preserve-search="true"
@@ -18,3 +26,4 @@ const placeholder = ref("");
   >
   </multiselect>
 </template>
+<style src="@vueform/multiselect/themes/default.css"></style>
