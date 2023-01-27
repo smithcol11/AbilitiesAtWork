96fc30be308453c53918e5ebde89ad34bfa3b68a
benttruong <108073148+benttruong@users.noreply.github.com>
2023-01-27 01:58:25 -0800

    deleted some comments

--- backend/routes/add_job.js
+++ backend/routes/add_job.js
@@ -3,33 +3,11 @@
 const express = require("express");
 const router = express.Router();
 const Job = require("../schema/job");
-// const session = require("express-session");
-// const cors = require("cors");
-// const MongoStore = require("connect-mongo");
-// // const jwt = require(jsonwebtoken);
-
-
-// router.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI }),
-//   })
-// );
-
-// router.use(
-//   cors({
-//     origin: "http://localhost:8080",
-//     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-//     credentials: true,
-//   })
-// );
 
 // Add Job
 router.post("/createJob", async (req, res) => {
   // console.log(req.body);
-  await Job.insertMany({
+  await Job.insertOne({
     contactName: req.body.contactName,
     businessName: req.body.businessName,
     industry: req.body.industry,
