0a9f4f4788de454683a97e8e34c2274d245a0ac0
benttruong <108073148+benttruong@users.noreply.github.com>
2023-01-31 02:23:29 -0800

    added some comments for the Unexpected end of JSON input issue while T/S

--- backend/routes/add_job.js
+++ backend/routes/add_job.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { json } = require("express");
 const express = require("express");
 const router = express.Router();
 const Job = require("../schema/job");
@@ -23,7 +24,16 @@ router.post("/createJob", async (req, res) => {
     contactEmail: req.body.contactEmail,
     contactPhoneNumber: req.body.contactPhoneNumber,
   });
+
+  // the line below would create an exception for Unexpected end of JSON input
+  // but still give the correct status 201. There is something wrong with res that seems
+  // to trigger Unexpected end of JSON
+
+  // if we are not sending anything back, the request still successfully add new
+  // data to database but will result in Failed to fetch error for a timeout pending fetch
+
   res.status(201).send();
+
 });
 
 module.exports = router;
