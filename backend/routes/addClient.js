4f9d25ffddc43432bdc27c759e91eb027857c455
Jesse Olsen-Jacobsen <jolsen2@pdx.edu>
2023-01-31 21:54:50 -0800

    updated addClient.js

--- backend/routes/addClient.js
+++ backend/routes/addClient.js
@@ -1,43 +1,44 @@
+"use strict";
+
 const express = require("express");
 const router = express.Router();
 const Client = require("../schema/client");
-const session = require("express-session");
-const cors = require("cors");
-const MongoStore = require("connect-mongo");
-const jwt = require("jsonwebtoken");
 
-router.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI}),
-    })
-);
+// router.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         resave: false,
+//         saveUninitialized: false,
+//         store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI}),
+//     })
+// );
 
-router.use(
-    cors({
-        origin: "http://localhost:8080",
-        methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-        credentials: true,
-    })
-);
+// router.use(
+//     cors({
+//         origin: "http://localhost:8080",
+//         methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+//         credentials: true,
+//     })
+// );
 
-router.post("/verifyJWT", (req, res) => {
-    if (req.cookies.token) {
-      let result = jwt.verify(req.cookies.token, process.env.SESSION_SECRET);
-      if (result == "admin") {
-        res.json({ auth: true, admin: true });
-      } else if (result) {
-        res.json({ auth: true, admin: false });
-      } else res.json({ auth: false, admin: false });
-    }
+// router.post("/verifyJWT", (req, res) => {
+//     if (req.cookies.token) {
+//       let result = jwt.verify(req.cookies.token, process.env.SESSION_SECRET);
+//       if (result == "admin") {
+//         res.json({ auth: true, admin: true });
+//       } else if (result) {
+//         res.json({ auth: true, admin: false });
+//       } else res.json({ auth: false, admin: false });
+//     }
+//   });
+
+  router.post("/addClient", async (req, res) => {
+    await Client.insertOne({
+      hours: req.body.clientHours,
+      intials: req.body.clientInitials,
+      industry: req.body.clientIndustry
+    });
+    res.status(201).send();
   });
 
-  router.post("/addClient", (req, res) => {
-    Client.register(
-        new Client({
-            userID: 
-        })
-    )
-  })
+  module.exports = router;
