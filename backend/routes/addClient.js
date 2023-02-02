ddf9597fe0622243fcc785f04b87198a2b015d75
Jesse Olsen-Jacobsen <jolsen2@pdx.edu>
2023-02-01 18:02:12 -0800

    updated addClient.js to include necessary form information

--- backend/routes/addClient.js
+++ backend/routes/addClient.js
@@ -32,13 +32,13 @@ const Client = require("../schema/client");
 //     }
 //   });
 
-  router.post("/addClient", async (req, res) => {
-    await Client.insertOne({
-      hours: req.body.clientHours,
-      intials: req.body.clientInitials,
-      industry: req.body.clientIndustry
-    });
-    res.status(201).send();
+router.post("/addClient", async (req, res) => {
+  await Client.insertOne({
+    hours: req.body.clientHours,
+    intials: req.body.clientInitials,
+    industry: req.body.clientIndustry
   });
+  res.status(201).send();
+});
 
-  module.exports = router;
+module.exports = router;
