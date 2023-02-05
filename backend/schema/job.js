c0580e39d92e36043d988b87987e637b977cf3c2
Cesium1289 <Carson.hansen@gmail.com>
2023-02-05 14:45:46 -0800

    Removed comments on job schema

--- backend/schema/job.js
+++ backend/schema/job.js
@@ -17,34 +17,6 @@ const jobSchema = new Schema(
     notes: { type: String },
     contactEmail: { type: String, required: true },
     contactPhoneNumber: { type: String, required: true },
-
-    // id: { type: String, required: true },
-    // postedBy: { type: String, required: true },
-    // postedOn: { type: Date, required: true },
-    // editedBy: { type: String, required: true },
-    // editedOn: { type: Date, required: true },
-
-    // detail: {
-    //   category: { type: String, required: true },
-    //   type: { type: String, enum: ["Full-Time", "Part-Time"], required: true }, // this was originally worded "Hours per W" from the schema document
-    //   shift: {
-    //     // start times
-    //     type: String,
-    //     enum: [
-    //       "Early", // early, before 9
-    //       "Morning", // 9-12
-    //       "Afternoon", // 12-5
-    //       "Evening", // late, after 5
-    //     ],
-    //     required: true,
-    //   },
-    //   pay: { type: Number, required: true },
-    //   benefits: { type: String },
-    //   notes: { type: String }, //description
-    // },
-    // address: { type: String, required: true },
-    // city: { type: String, required: true },
-    // county: { type: String, required: true },
   },
   { timestamps: true }
 );
