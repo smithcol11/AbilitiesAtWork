f6c4240e2f805dfb0e71803a19973c423b3f8618
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-02-04 21:03:01 -0800

    add route to server.js

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,7 @@ app.use(passport.session());
 app.use(require("./routes/authAdmin.js"));
 app.use(require("./routes/authEmployee.js"));
 app.use(require("./routes/getJobs.js"));
+app.use(require("./routes/clientTasks.js"));
 const server = app.listen(port, () => {
   console.log(`AAW app listening on port ${port}`);
 });
