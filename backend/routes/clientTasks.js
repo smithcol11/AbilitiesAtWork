9b602d3eacb36119807ccb17c2bab18d548093b4
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-02-05 17:41:36 -0800

    update routes'name, suggest to edit and remove by full name instead of initials

--- backend/routes/clientTasks.js
+++ backend/routes/clientTasks.js
@@ -4,9 +4,9 @@ const express = require("express");
 const router = express.Router();
 const Client = require("../schema/client")
 
-router.put('/edit/:initials', async (req, res) => {
+router.put('/editClient/:fullName', async (req, res) => {
   try {
-    const client = await Client.findOneAndUpdate({ initials: req.params.initials }, req.body, { new: true });
+    const client = await Client.findOneAndUpdate({ fullName: req.params.fullName }, req.body, { new: true });
     if (!client) {
       return res.status(404).send({ error: 'Client not found' });
     }
@@ -17,9 +17,9 @@ router.put('/edit/:initials', async (req, res) => {
   }
 });
 
-router.delete('/delete/:initials', async (req, res) => {
+router.delete('/deleteClient/:fullName', async (req, res) => {
   try {
-    const client = await Client.findOneAndRemove({ initials: req.params.initials });
+    const client = await Client.findOneAndRemove({ fullName: req.params.fullName });
     if (!client) {
       return res.status(404).send({ error: 'Client not found' });
     }
