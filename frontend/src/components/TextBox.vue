54d0a432d0da5634d0dda7c701b6ae3e0f766afb
Jacob Padgett <jacobapadgett@gmail.com>
2023-02-13 19:35:23 -0800

    Added optional maxLength prop to TextBox vue

--- frontend/src/components/TextBox.vue
+++ frontend/src/components/TextBox.vue
@@ -6,7 +6,7 @@ const props = defineProps({
   },
   placeholder: {
     type: String,
-    default: "Place holder text",
+    default: "Place holnullder text",
   },
   modelValue: {
     type: String,
@@ -15,6 +15,10 @@ const props = defineProps({
   label: {
     type: String,
     default: null,
+  },
+  maxLength: {
+    type: String,
+    default: "524288",
   },
 });
 </script>
@@ -29,6 +33,7 @@ const props = defineProps({
     class="form-control block w-full px-3 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-accentLight rounded"
     :placeholder="placeholder"
     :value="modelValue"
+    :maxlength="maxLength"
     @input="$emit('update:modelValue', $event.target.value)"
   />
 </template>
