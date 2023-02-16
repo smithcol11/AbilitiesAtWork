f8de81711b03a524aee972f50f1c7513bf2ffe70
MinhTranPDX <50005387+minhtran02202@users.noreply.github.com>
2023-02-16 14:52:01 -0800

    update addClient and GetAllJobs to have try and catch block

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -5,16 +5,20 @@ const router = express.Router();
 const Client = require("../schema/client");
 
 router.post("/addClient", async (req, res) => {
-  await Client.insertMany({
-    firstName: req.body.firstName,
-    middleInitial: req.body.middleInitial,
-    lastInitial: req.body.lastInitial,
-    hours: req.body.hours,
-    industry: req.body.industry,
-    enteredBy: "FIX ME",
-    updatedBy: "FIX ME",
-  });
-  res.status(201).send();
+  try {
+    await Client.insertMany({
+      firstName: req.body.firstName,
+      middleInitial: req.body.middleInitial,
+      lastInitial: req.body.lastInitial,
+      hours: req.body.hours,
+      industry: req.body.industry,
+      enteredBy: "FIX ME",
+      updatedBy: "FIX ME",
+    });
+    res.status(201).send();
+  } catch (error){
+    res.status(500).send({error: "Error adding client"})
+  }
 });
 
 router.put("/editClient", async (req, res) => {
@@ -28,6 +32,7 @@ router.put("/editClient", async (req, res) => {
       req.body,
       { new: true }
     );
+    
     if (!client) {
       return res.status(404).send({ error: "Client not found when updating" });
     }
@@ -45,6 +50,7 @@ router.delete("/deleteClient", async (req, res) => {
       middleInitial: req.body.middleInitial,
       lastInitial: req.body.lastInitial,
     });
+
     if (!client) {
       return res.status(404).send({ error: "Client not found when deleting" });
     }
--- backend/routes/job.js
+++ backend/routes/job.js
@@ -4,39 +4,50 @@ const Job = require("../schema/job");
 
 // Add Job
 router.post("/createJob", async (req, res) => {
-  await Job.insertMany({
-    contact: {
-      email: req.body.contactEmail,
-      name: req.body.contactName,
-      phone: req.body.contactPhoneNumber,
-    },
-    employer: req.body.businessName,
-    industry: req.body.industry,
-    position: req.body.position,
-    shift: req.body.shift,
-    timeCommitment: req.body.hours,
-    city: req.body.city,
-    zip: req.body.zip,
-    openingDate: req.body.date,
-    address: req.body.address,
-    county: req.body.county,
-    notes: req.body.notes,
-    hourlyWage: 0,
-    enteredBy: "FIX ME",
-    updatedBy: "FIX ME",
-    benefits: "FIX ME",
-  });
-
-  res.status(201).send();
+  try{
+    await Job.insertMany({
+      contact: {
+        email: req.body.contactEmail,
+        name: req.body.contactName,
+        phone: req.body.contactPhoneNumber,
+      },
+      employer: req.body.businessName,
+      industry: req.body.industry,
+      position: req.body.position,
+      shift: req.body.shift,
+      timeCommitment: req.body.hours,
+      city: req.body.city,
+      zip: req.body.zip,
+      openingDate: req.body.date,
+      address: req.body.address,
+      county: req.body.county,
+      notes: req.body.notes,
+      hourlyWage: 0,
+      enteredBy: "FIX ME",
+      updatedBy: "FIX ME",
+      benefits: "FIX ME",
+    });
+    res.status(201).send();
+  } catch(error){
+    res.status(500).send('Error creating job')
+  }
 });
 
-router.get("/GetAllJobs", (req, res) => {
-  Jobs.find({})
-    .then((data) => {
+router.get("/GetAllJobs", async (req, res) => {
+  try{
+    await Job.find({});
+    if (!Job){
+      return res.status(400).send('No job found when searching')
+    }
+    res.send(Job)
+  } catch(error){
+    res.status(500).send('Error finding jobs')
+  }
+    /*.then((data) => {
       console.log(data);
       res.json(data);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err));*/
 });
 
 module.exports = router;
