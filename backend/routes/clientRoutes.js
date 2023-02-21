6291c10145a88b706bce0ca4948ddf1e7ba4bfbf
Richard Duffy <rduffy@pdx.edu>
2023-02-21 11:14:28 -0800

    backend call

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -55,4 +55,13 @@ router.delete("/deleteClient", async (req, res) => {
   }
 });
 
+router.get("/GetAllClients", (req, res) => {
+  Client.find({})
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((err) => console.log(err));
+});
+
 module.exports = router;
