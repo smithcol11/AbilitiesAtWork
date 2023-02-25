61c6bdb5e327726cc08a3fd2b1d70eee58568aab
Colin <smithcol@pdx.edu>
2023-02-25 14:00:30 -0800

    Changed backend matching routes.

--- backend/routes/getClient.js
+++ backend/routes/getClient.js
@@ -3,10 +3,11 @@
 const express = require("express");
 const router = express.Router();
 const Client = require("../schema/client");
+const Job = require("../schema/job");
 
-const baseURL = 'http://localhost:3000'
+const baseURL = "http://localhost:3000";
 function matchInitials(client) {
-  return client.body.initials === 'JAR';
+  return client.body.initials === "JAR";
 }
 
 /*
@@ -25,53 +26,48 @@ router.get("/matchClient", async (req, res) => {
 */
 
 router.post("/matchClient", async (req, res) => {
-  let matchedClient = await fetch(baseURL + '/getClient', {
+  let matchedClient = await fetch(baseURL + "/getClient", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
     body: JSON.stringify({
       firstName: req.body.firstName,
       middleInitial: req.body.middleInitial,
-      lastInitial: req.body.lastInitial
-    })
+      lastInitial: req.body.lastInitial,
+    }),
   });
-  
 
-  
   if (!matchedClient) {
-
     try {
       matchedClient = await matchedClient.json();
-      console.log("Found client: ", matchedClient)
-    } catch(error) {
-      console.log("Could not find client with those credentials ", error)
-      res.json("[]")
+      console.log("Found client: ", matchedClient);
+    } catch (error) {
+      console.log("Could not find client with those credentials ", error);
+      res.json("[]");
     }
-    
-    let matchedJobs = await fetch(baseURL + '/getJobs', {
+
+    let matchedJobs = await fetch(baseURL + "/getJobs", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
       body: JSON.stringify({
         industry: matchedClient.industry,
-        timeCommitment: matchedClient.hours
-      })
-    });  
-  
-    try {
-        matchedJobs = await matchedJobs.json();
-        console.log("Matched jobs: ", matchedJobs)
-        res.json(matchedJobs)
-      } catch(error) {
-        console.log(error)
-        res.json("[]")
-      }
-  }
-  else {
-    console.log("Could not find client with those credentials ")
-  }
+        timeCommitment: matchedClient.hours,
+      }),
+    });
 
-})
+    try {
+      matchedJobs = await matchedJobs.json();
+      console.log("Matched jobs: ", matchedJobs);
+      res.json(matchedJobs);
+    } catch (error) {
+      console.log(error);
+      res.json("[]");
+    }
+  } else {
+    console.log("Could not find client with those credentials ");
+  }
+});
 /*
 router.get("/getClient", (req, res) => {
   Client.find({})
@@ -87,17 +83,24 @@ router.get("/getClient", (req, res) => {
 */
 
 router.post("/getClient", async (req, res) => {
-  let matchedClient = await Client.find({}).then((data) => {
-    let match = data.find(({firstName, middleInitial, lastInitial}) =>
-    firstName === req.body.firstName &&
-    middleInitial === req.body.middleInitial &&
-    lastInitial === req.body.lastInitial
-    )
-
-    return match
+  let matchingJobs = [];
+  await Client.find({
+    firstName: req.body.firstName,
+    middleInitial: req.body.middleInitial,
+    lastInitial: req.body.lastInitial,
+  }).then((client) => {
+    if (client.length == 0) return;
+    for (let i in client[0].industry) {
+      Job.find({
+        industry: client[0].industry[i],
+        timeCommitment: client[0].hours,
+      }).then((jobs) => {
+        matchingJobs = matchingJobs.concat(jobs);
+        console.log(matchingJobs);
+      });
+    }
+    console.log(matchingJobs);
   });
-  res.json(matchedClient);
 });
-
 
 module.exports = router;
