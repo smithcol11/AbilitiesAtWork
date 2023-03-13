27bad49ef59de19f7b8852141d516750678f0bcd
Cesium1289 <Carson.hansen@gmail.com>
2023-03-13 11:18:39 -0700

    Updated job options

--- backend/routes/jobOptions.js
+++ backend/routes/jobOptions.js
@@ -10,7 +10,23 @@ router.get("/GetJobOptions", async (req, res) => {
         .status(400)
         .send({ error: "No job option found when searching" });
     }
-    res.json(option[0]);
+
+    const formattedOption = {
+      ...option[0]._doc,
+      positions: option[0].positions.map((position) =>
+        position
+          .split(" ")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")
+      ),
+      industries: option[0].industries.map((industry) =>
+        industry
+          .split(" ")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")
+      ),
+    };
+    res.json(formattedOption);
   } catch (error) {
     res.status(500).send({ error: "Error getting job options" });
   }
@@ -24,8 +40,10 @@ router.post("/updateJobOptions", async (req, res) => {
         counties: req.body.counties,
         cities: req.body.cities,
         zips: req.body.zips,
-        positions: req.body.positions.toLowerCase(),
-        industries: req.body.industries.toLowerCase(),
+        positions: req.body.positions.map((position) => position.toLowerCase()),
+        industries: req.body.industries.map((industry) =>
+          industry.toLowerCase()
+        ),
         shiftOptions: req.body.shiftOptions,
         timeCommitmentOptions: req.body.timeCommitmentOptions,
       }
