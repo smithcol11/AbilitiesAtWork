fbe4507db5b4014575d087599b81230aa1339744
Colin Smith <smithcol@pdx.edu>
2023-03-21 11:23:21 -0700

    Fix matching and editing bug caused by capitals

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -11,7 +11,7 @@ router.post("/addClient", async (req, res) => {
       middleInitial: req.body.middleInitial.toLowerCase(),
       lastInitial: req.body.lastInitial.toLowerCase(),
       hours: req.body.hours,
-      industry: req.body.industry.toLowerCase(),
+      industry: req.body.industry.map((industry) => industry.toLowerCase()),
       enteredBy: "FIX ME",
       updatedBy: "FIX ME",
     });
@@ -23,6 +23,13 @@ router.post("/addClient", async (req, res) => {
 
 router.put("/editClient", async (req, res) => {
   try {
+    req.body.data.firstName = req.body.data.firstName.toLowerCase();
+    req.body.data.middleInitial = req.body.data.middleInitial.toLowerCase();
+    req.body.data.lastInitial = req.body.data.lastInitial.toLowerCase();
+    req.body.data.industry = req.body.data.industry.map((industry) =>
+      industry.toLowerCase()
+    );
+    console.log(req.body);
     const client = await Client.updateOne(
       {
         firstName: req.body.initialData.firstName.toLowerCase(),
